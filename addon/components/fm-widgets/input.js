d50bd75238695489477e77c6e7a5aeb13d2fda77
Léo Renaud-Allaire <leo.renaud@hooktstudios.com>
2018-08-28 16:45:19 -0400

    Allow maxlength attribute on input

--- addon/components/fm-widgets/input.js
+++ addon/components/fm-widgets/input.js
@@ -4,6 +4,7 @@ const {oneWay} = Ember.computed;
 export default Ember.TextField.extend({
   placeholder: oneWay('widgetAttrs.placeholder'),
   name: oneWay('widgetAttrs.name'),
+  maxlength: oneWay('widgetAttrs.maxlength'),
   disabled: Ember.computed.oneWay('widgetAttrs.disabled'),
 
   type: Ember.computed('widgetAttrs.type', function(){
